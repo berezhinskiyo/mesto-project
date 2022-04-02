4eadeadd4c029eddb1aac86f0a1bc3bc4f19ddbd
ob <oberezhinskiy@gmail.com>
2022-04-02 20:56:21 +0300

    validation, version 3

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import './pages/index.css'; // добавьте импорт главного файла стилей 
 import * as card from './components/card';
 import * as validator from './components/validate';
-card.init();
+card.initializeCards();
 validator.enableValidation(validator.validationConfig);
 
 
